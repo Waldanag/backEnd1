c982b902599e2b89081194f7ee33696ca6b2deed
Waldanag <walter@algabits.com>
2023-11-27 00:14:14 -0300

    trabajando excepciones

---
+++ clinicaOdontologicaW2/src/main/java/com/backend/clinicaOdontologicaW2/exceptions/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.backend.clinicaOdontologicaW2.exceptions;
+
+public class BadRequestException extends Exception{
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
--- clinicaOdontologicaW2/src/main/java/com/backend/clinicaOdontologicaW2/exceptions/GlobalExceptionHandler.java
+++ clinicaOdontologicaW2/src/main/java/com/backend/clinicaOdontologicaW2/exceptions/GlobalExceptionHandler.java
@@ -22,6 +22,14 @@ public class GlobalExceptionHandler {
         return mensaje;
     }
 
+    @ExceptionHandler({BadRequestException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> manejarBadRequest(BadRequestException exception){
+        Map<String, String> mensaje = new HashMap<>();
+        mensaje.put("mensaje", "Recurso no encontrado: " + exception.getMessage());
+        return mensaje;
+    }
+
     @ExceptionHandler({MethodArgumentNotValidException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> procesarValidationException(MethodArgumentNotValidException exception) {
