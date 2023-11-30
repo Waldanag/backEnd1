85800ba796422ec0193a58ba0817d259c8bf8e9a
Waldanag <walter@algabits.com>
2023-11-29 23:42:42 -0300

    Agregando swagger a los controlles

--- clinicaOdontologicaW2/src/main/java/com/backend/clinicaOdontologicaW2/controller/PacienteController.java
+++ clinicaOdontologicaW2/src/main/java/com/backend/clinicaOdontologicaW2/controller/PacienteController.java
@@ -2,10 +2,16 @@ package com.backend.clinicaOdontologicaW2.controller;
 
 import com.backend.clinicaOdontologicaW2.dto.entrada.paciente.PacienteEntradaDto;
 import com.backend.clinicaOdontologicaW2.dto.modificacion.PacienteModificacionEntradaDto;
+import com.backend.clinicaOdontologicaW2.dto.salida.odontologo.OdontologoSalidaDto;
 import com.backend.clinicaOdontologicaW2.dto.salida.paciente.PacienteSalidaDto;
 import com.backend.clinicaOdontologicaW2.entity.Paciente;
 import com.backend.clinicaOdontologicaW2.exceptions.ResourceNotFoundException;
 import com.backend.clinicaOdontologicaW2.service.IPacienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,36 +21,93 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/pacientes")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class PacienteController {
-    private IPacienteService pacienteService;
+    private final IPacienteService pacienteService;
     public PacienteController(IPacienteService pacienteService) {
         this.pacienteService = pacienteService;
     }
 
     //POST
+    @Operation(summary = "Registro de un nuevo paciente")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Paciente guardado correctamente",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PacienteSalidaDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Server error",
+                    content = @Content)
+    })
     @PostMapping("/registrar")
     public ResponseEntity<PacienteSalidaDto> registrarPaciente(@RequestBody @Valid PacienteEntradaDto paciente) {
         return new ResponseEntity<>(pacienteService.registrarPaciente(paciente), HttpStatus.CREATED);
     }
 
     //GET
+    @Operation(summary = "Búsqueda de un paciente por Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Paciente obtenido correctamente",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PacienteSalidaDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Id inválido",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Paciente no encontrado",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Server error",
+                    content = @Content)
+    })
     @GetMapping("{id}")
     public ResponseEntity<PacienteSalidaDto> obtenerPacientePorId(@PathVariable Long id) {
         return new ResponseEntity<>(pacienteService.buscarPacientePorId(id), HttpStatus.OK);
     }
+
+    @Operation(summary = "Listado de todos los pacientes")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Listado de pacientes obtenido correctamente",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PacienteSalidaDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Server error",
+                    content = @Content)
+    })
     @GetMapping("/listar")
     public ResponseEntity<List<PacienteSalidaDto>> listarPacientes() {
         return new ResponseEntity<>(pacienteService.listarPacientes(), HttpStatus.OK);
     }
 
     //PUT
+    @Operation(summary = "Actualización de un paciente")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Paciente actualizado correctamente",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PacienteSalidaDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Paciente no encontrado",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "UServer error",
+                    content = @Content)
+    })
     @PutMapping("/actualizar")
     public PacienteSalidaDto actualizarPaciente(@RequestBody PacienteModificacionEntradaDto paciente) {
         return pacienteService.actualizarPaciente(paciente);
     }
 
     //DELETE
+    @Operation(summary = "Eliminación de un paciente por Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Paciente eliminado correctamente",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = String.class))}),
+            @ApiResponse(responseCode = "400", description = "Id inválido",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Paciente no encontrado",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Server error",
+                    content = @Content)
+    })
     @DeleteMapping("eliminar/{id}")
     public ResponseEntity<?> eliminarPaciente(@PathVariable Long id) throws ResourceNotFoundException {
         pacienteService.eliminarPaciente(id);
--- clinicaOdontologicaW2/src/main/java/com/backend/clinicaOdontologicaW2/controller/TurnoController.java
+++ clinicaOdontologicaW2/src/main/java/com/backend/clinicaOdontologicaW2/controller/TurnoController.java
@@ -6,6 +6,11 @@ import com.backend.clinicaOdontologicaW2.dto.salida.turno.TurnoSalidaDto;
 import com.backend.clinicaOdontologicaW2.exceptions.BadRequestException;
 import com.backend.clinicaOdontologicaW2.exceptions.ResourceNotFoundException;
 import com.backend.clinicaOdontologicaW2.service.ITurnoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,36 +22,92 @@ import java.util.List;
 @RestController
 @RequestMapping("/turnos")
 public class TurnoController {
-    private ITurnoService turnoService;
+    private final ITurnoService turnoService;
 
     public TurnoController(ITurnoService turnoService){
         this.turnoService = turnoService;
     }
 
     //POST
+    @Operation(summary = "Registro de un nuevo turno")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Turno guardado correctamente",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = TurnoSalidaDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Server error",
+                    content = @Content)
+    })
     @PostMapping("/registrar")
     public ResponseEntity<TurnoSalidaDto> registrarTurno(@RequestBody @Valid TurnoEntradaDto turno) throws BadRequestException {
         return new ResponseEntity<>(turnoService.registrarTurno(turno), HttpStatus.CREATED);
     }
 
     //GET
+    @Operation(summary = "Búsqueda de un turno por ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Turno encontrado correctamente",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = TurnoSalidaDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Turno no encontrado",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "UServer error",
+                    content = @Content)
+    })
     @GetMapping("{id}")
     public ResponseEntity<TurnoSalidaDto> obtenerTurnoPorId(@PathVariable Long id){
         return new ResponseEntity<>(turnoService.buscarTurnoPorId(id), HttpStatus.OK);
     }
 
+    @Operation(summary = "Listado de todos los turnos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Listado de turnos obtenido correctamente",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = TurnoSalidaDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Server error",
+                    content = @Content)
+    })
     @GetMapping("/listar")
     public ResponseEntity<List<TurnoSalidaDto>> listarTurnos(){
         return new ResponseEntity<>(turnoService.listarTurnos(), HttpStatus.OK);
     }
 
     //PUT
+    @Operation(summary = "Actualización de un turno")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Turno actualizado correctamente",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = TurnoSalidaDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Turno no encontrado",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "UServer error",
+                    content = @Content)
+    })
     @PutMapping("/actualizar")
     public TurnoSalidaDto actualizarTurno(@RequestBody TurnoModificacionEntradaDto turno) throws ResourceNotFoundException {
         return turnoService.actualizarTurno(turno);
     }
 
     //DELETE
+    @Operation(summary = "Eliminación de un turno por Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Turno eliminado correctamente",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = String.class))}),
+            @ApiResponse(responseCode = "400", description = "Id inválido",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Turno no encontrado",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Server error",
+                    content = @Content)
+    })
     @DeleteMapping("/{id}")
     public void EliminarTurno(@PathVariable Long id) throws ResourceNotFoundException {
         turnoService.eliminarTurno(id);
